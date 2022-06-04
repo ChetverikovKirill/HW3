83e710bf1446b351a2d808adcc9bd418b0a981c8
Kirill Chetverikov <MaraZmOo@yandex.ru>
2022-06-04 17:10:23 +0300

    пушим ветку,добавил класс констурктор с названием типом и цветои и enum "Type" с перечислением типов мебели

---
+++ src/Type.java
@@ -0,0 +1,16 @@
+public enum Type {
+    BED("Кровать"),
+    WARDROBE("Шкаф"),
+    TABLE("Стол"),
+    CHAIR("Стул");
+
+    String type;
+
+
+
+
+    Type(String type){
+        this.type = type;
+
+    }
+}
